6aef15237b97757083ba32054376b175f744ffea
adammartin <ebondark.od@gmail.com>
2019-01-03 10:07:31 -0500

    Working chain ... all hardcoded but now manageable

--- aks_cluster.sh
+++ aks_cluster.sh
@@ -6,7 +6,8 @@ SUBSCRIPTION=ApplicationDevelopment_DEV
 LOCATION=eastus2
 RESOURCE_GROUP=SharedApplicationCluster
 CLUSTER_NAME=azshrappaks01d
-NODE_COUNT=1
+NODE_COUNT=2
+DELETE_CUR_CREDENTIALS=true
 
 az account set -s $SUBSCRIPTION
 echo "EXECUTING SCRIPTS FOR THE FOLLOWING SUBSCRIPTIONS:"
@@ -18,6 +19,10 @@ echo "CREATING/UPDATING AKS CLUSTER:"
 az aks create --subscription $SUBSCRIPTION --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --node-count $NODE_COUNT --generate-ssh-keys
 
 # NOTE: You can run into a pain point if your local ~/.kube/config file already has the same named cluster in place.
+if [ $DELETE_CUR_CREDENTIALS = true ] ; then
+  echo "DELETING ~/.kube/config"
+  rm ~/.kube/config
+fi
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
 kubectl get nodes
 
@@ -25,10 +30,14 @@ kubectl get nodes
 # Please note: by default, Tiller is deployed with an insecure 'allow unauthenticated users' policy.
 # To prevent this, run `helm init` with the --tiller-tls-verify flag.
 # For more information on securing your installation see: https://docs.helm.sh/using_helm/#securing-your-helm-installation
+echo "INSTALLING HELM"
 kubectl apply -f helm-rbac.yaml
 helm init --service-account tiller
 helm repo update
 
-#helm install stable/nginx-ingress --namespace kube-system <---- this worked!
-helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=$NODE_COUNT
+echo "INSTALLING nginx-ingress"
+helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=$NODE_COUNT --version 0.23.0
 kubectl get service -l app=nginx-ingress --namespace kube-system
+
+
+# Continue from: https://docs.microsoft.com/en-us/azure/aks/ingress-tls#configure-a-dns-name
---
+++ configure_certificates.sh
@@ -0,0 +1,3 @@
+# Right now certificates.yaml is hardcoded!!! Need to figure out how to auto generate that sucker
+
+kubectl apply -f certificates.yaml
