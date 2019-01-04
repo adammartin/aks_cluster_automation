a14df3ee86aa035cf243de4e3c7cb0ff31b8a4e3
adammartin <ebondark.od@gmail.com>
2019-01-04 08:35:26 -0500

    Trying to clean up my project

--- aks_cluster.sh
+++ aks_cluster.sh
@@ -3,13 +3,20 @@ set -e
 
 # VARIABLES TO EXTRACT
 SUBSCRIPTION=ApplicationDevelopment_DEV
-LOCATION=eastus2
-RESOURCE_GROUP=SharedApplicationCluster
-CLUSTER_NAME=azshrappaks01d
+RESOURCE_GROUP=SharedApplicationCluster2
+DNSNAME="dev2-mednax-kubernetes"
+CLUSTER_NAME=azshrappaks02d
+LOCATION=eastus
 NODE_COUNT=2
-DNSNAME="dev-mednax-kubernetes"
 CERT_PROVIDER="letsencrypt-prod" # letsencrypt-staging
-DELETE_CUR_CREDENTIALS="true"
+DELETE_CUR_CREDENTIALS="false"
+
+
+function get_ip()
+{
+  kubectl get service -l app=nginx-ingress --namespace kube-system | awk 'FNR == 2 {print $4}'
+}
+
 
 function valid_ip()
 {
@@ -21,10 +28,43 @@ function valid_ip()
     fi
 }
 
-function get_ip()
+
+function install_ingress()
 {
-  kubectl get service -l app=nginx-ingress --namespace kube-system | awk 'FNR == 2 {print $4}'
+  {
+    helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=$NODE_COUNT --version 0.23.0
+  } || {
+    echo "TILLER NOT READY"
+  }
 }
+
+
+function retrieve_credentials()
+{
+  # NOTE: You can run into a pain point if your local ~/.kube/config file already has the same named cluster in place from previous instance.
+  local DELETE_CREDS=$1
+  local L_RESOURCE_GROUP=$2
+  local L_CLUSTER_NAME=$3
+  if [ $DELETE_CREDS = "true" ] ; then
+    echo "DELETING ~/.kube/config"
+    rm ~/.kube/config
+  fi
+  az aks get-credentials --resource-group $L_RESOURCE_GROUP --name $L_CLUSTER_NAME
+  kubectl get nodes
+}
+
+
+function install_helm()
+{
+  # Please note: by default, Tiller is deployed with an insecure 'allow unauthenticated users' policy.
+  # To prevent this, run `helm init` with the --tiller-tls-verify flag.
+  # For more information on securing your installation see: https://docs.helm.sh/using_helm/#securing-your-helm-installation
+  echo "INSTALLING HELM"
+  kubectl apply -f helm-rbac.yaml
+  helm init --service-account tiller
+  helm repo update
+}
+
 
 az account set -s $SUBSCRIPTION
 echo "EXECUTING SCRIPTS FOR THE FOLLOWING SUBSCRIPTIONS:"
@@ -35,34 +75,24 @@ echo "CREATING/UPDATING AKS CLUSTER:"
 # --subscription $SUBSCRIPTION --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --node-count $NODE_COUNT --enable-addons monitoring --generate-ssh-keys
 az aks create --subscription $SUBSCRIPTION --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --node-count $NODE_COUNT --generate-ssh-keys
 
-# NOTE: You can run into a pain point if your local ~/.kube/config file already has the same named cluster in place.
-if [ $DELETE_CUR_CREDENTIALS = "true" ] ; then
-  echo "DELETING ~/.kube/config"
-  rm ~/.kube/config
-fi
-az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
-kubectl get nodes
+retrieve_credentials $DELETE_CUR_CREDENTIALS $RESOURCE_GROUP $CLUSTER_NAME
+install_helm
 
-
-# Please note: by default, Tiller is deployed with an insecure 'allow unauthenticated users' policy.
-# To prevent this, run `helm init` with the --tiller-tls-verify flag.
-# For more information on securing your installation see: https://docs.helm.sh/using_helm/#securing-your-helm-installation
-echo "INSTALLING HELM"
-kubectl apply -f helm-rbac.yaml
-helm init --service-account tiller
-helm repo update
-
-echo "PAUSING 10 SECONDS FOR AZURE TO SPIN UP"
-sleep 10
+#echo "PAUSING 30 SECONDS FOR AZURE TO SPIN UP"
+#sleep 30
 
 echo "INSTALLING nginx-ingress"
-helm install stable/nginx-ingress --namespace kube-system --set controller.replicaCount=$NODE_COUNT --version 0.23.0
-IP=kubectl get service -l app=nginx-ingress --namespace kube-system | awk 'FNR == 2 {print $4}'
+while [ "$(install_ingress)" = "TILLER NOT READY" ]
+do
+  echo "WAITING FOR INGRESS INSTALLATION"
+  ((ing_count++)) && ((ing_count==12)) && ing_count=0 && break # STOP AFTER 2 MINUTES
+  sleep 10
+done
 
 while [ "$(valid_ip)" != "VALID" ]
 do
   echo "WAITING FOR IP: $(get_ip)"
-  ((count++)) && ((count==18)) && count=0 && break # STOP AFTER 3 MINUTES
+  ((ip_count++)) && ((ip_count==18)) && ip_count=0 && break # STOP AFTER 3 MINUTES
   sleep 10
 done
 
@@ -85,4 +115,3 @@ else
   echo "APPLYING LETSENCRYPT PROD"
   kubectl apply -f cluster-issuer-prod.yaml
 fi
-# Continue from: https://docs.microsoft.com/en-us/azure/aks/ingress-tls#configure-a-dns-name
